0122d7142ff8d8ffbdcaf7b0dfaacaba1201b9d8
Joey Stevens <joemacstevens@users.noreply.github.com>
2026-02-05 19:30:59 -0500

    Remove A/B/C labels from product cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -293,7 +293,6 @@
 							<path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
 						</svg>
 					</div>
-					<span class="capability-number">A</span>
 					<h3>Health monitoring &amp; management</h3>
 					<ul class="list">
 						<li>Blood pressure monitors (digital &amp; manual)</li>
@@ -309,7 +308,6 @@
 							<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
 						</svg>
 					</div>
-					<span class="capability-number">B</span>
 					<h3>Mobility &amp; safety equipment</h3>
 					<ul class="list">
 						<li>Walkers (standard &amp; rollators)</li>
@@ -326,7 +324,6 @@
 							<path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
 						</svg>
 					</div>
-					<span class="capability-number">C</span>
 					<h3>Specialized medical support</h3>
 					<ul class="list">
 						<li>Nebulizers &amp; respiratory therapy</li>
