79a986bb819d5a358f5adeaca57643710247787f
Noah Stevens <noah@ajeo.design>
2026-03-02 13:03:26 -0500

    fix: update phone + add email icon on all interior pages (about, blog, products)

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -31,7 +31,12 @@
 			<a href="/#faq">FAQ</a>
 		</nav>
 		<div class="header-cta">
-			<a href="tel:+12015550100" class="header-phone">(201) 555-0100</a>
+			<a href="mailto:customercare@bgclear.com" class="header-icon" aria-label="Email us" title="Email us">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="20" height="20">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+				</svg>
+			</a>
+			<a href="tel:+12017657171" class="header-phone">(201) 765-7171</a>
 			<a class="button button-primary button-header" href="/#request-call">Contact Us</a>
 		</div>
 		<button class="hamburger" aria-label="Open menu" aria-expanded={mobileNavOpen} onclick={() => mobileNavOpen = true}>
@@ -58,7 +63,7 @@
 		<a href="/#faq" onclick={() => mobileNavOpen = false}>FAQ</a>
 	</div>
 	<div class="mobile-nav-footer">
-		<a href="tel:+12015550100" class="mobile-nav-phone">(201) 555-0100</a>
+		<a href="tel:+12017657171" class="mobile-nav-phone">(201) 765-7171</a>
 		<a class="button button-primary" href="/#request-call" onclick={() => mobileNavOpen = false}>Contact Us</a>
 	</div>
 </nav>
